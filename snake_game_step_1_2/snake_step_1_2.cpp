e38a72e949ecce7be88c87f8ee23fb6a5d4e7606
lee <iamlyg9667@kookmin.ac.kr>
2024-06-05 21:22:12 +0900

    snake_step_1_2 완성본
    수정 사항:
    - 원래 테두리를 COLOR_PAIR 이용해서 색 블록으로 출력했는데 이렇게 하니 세로줄이
    출력이 안되는 오류가 있어 일단 문자로 변경했습니다
    - 함수, 변수 네이밍 변경 및 일부 구조를 변경했습니다
    - 다른 파일들은 무시하시고 .cpp 안에 있는 코드만 긁어서 사용하시면 됩니다
    - .txt 파일에 코드에 대한 설명 정리해 놓았습니다

--- snake_game_step_1_2/snake_step_1_2.cpp
+++ snake_game_step_1_2/snake_step_1_2.cpp
@@ -5,8 +5,8 @@
 using namespace std;
 
 // 자주 사용되는 설정값 상수로 define
-#define width_game_Display 50
-#define height_game_Display 80
+#define width_game_Display 80
+#define height_game_Display 40
 #define X_game_Display 1
 #define Y_game_Display 1
 
@@ -14,6 +14,8 @@ using namespace std;
 #define DOWN 2
 #define LEFT 3
 #define RIGHT 4
+
+#define init_Snake 3
 
 //구조체, 클래스 선언 공간
 struct Wall{
@@ -30,39 +32,62 @@ vector<Wall> data_Wall;
 int cur_Stage = 1;
 
 //함수 선언 공간
-void afterCrash(bool &setStop){
+void set_Snake(deque<Snake> &data){
+  for(int i=1; i < init_Snake+1; i++){
+    data.push_front(Snake(X_game_Display+i, Y_game_Display+1));
+  }
+}
+
+inline void afterCrash(bool &setStop){
   setStop = true;
 }
 
-bool crash_Check(int x, int y){
+inline bool crash_Check(int x, int y){
   for(Wall val : data_Wall){
     if (x == val.x && y == val.y){
       return true;
     }
-    if(x == X_game_Display || y == Y_game_Display || x >= height_game_Display || y >= width_game_Display){
+    if(x == X_game_Display || y == Y_game_Display || x >= height_game_Display-2 || y >= width_game_Display-2 ){
       return true;
     }
   }
   return false;
 }
 
+void map_1();
+void map_2();
+void map_3();
+void map_4();
+
 void draw_Wall(){
   switch (cur_Stage) {
     case 1: {
-      int tmp_x = 20;
-      int tmp_y = 20;
-      for(; tmp_x < 71; tmp_x++){
-        data_Wall.push_back(Wall(tmp_x, tmp_y));
-      }
+      data_Wall.clear();
+      map_1();
+      cur_Stage++;
       break;
     }
     case 2: {
+      data_Wall.clear();
+      map_2();
+      cur_Stage++;
       break;
     }
     case 3: {
+      data_Wall.clear();
+      map_3();
+      cur_Stage++;
       break;
     }
     case 4: {
+      data_Wall.clear();
+      map_4();
+      cur_Stage++;
+      break;
+    }
+    default: {
+      cur_Stage = 1;
+      draw_Wall();
       break;
     }
   }
@@ -77,7 +102,6 @@ int main()
   keypad(stdscr, true);
   timeout(300);
 
-  // color pair 설정 공간
   start_color();
   init_pair(1, COLOR_WHITE, COLOR_BLACK); //배경
   init_pair(5, COLOR_WHITE,COLOR_WHITE); // normal wall
@@ -96,14 +120,10 @@ int main()
 
   int cur_Dir = RIGHT; 
   int in_Key; 
-  int len_Snake = 3;
 
   // 함수 실행 공간
   draw_Wall();
-
-  // 초기값 설정 공간
-  for(int i=1; i < len_Snake+1; i++)
-    data_Snake.push_front(Snake(X_game_Display+i, Y_game_Display+1));
+  set_Snake(data_Snake);
 
   // while loop 진입
   while(!stop){
@@ -111,7 +131,7 @@ int main()
     Snake head = data_Snake.front();
     int head_x = head.x;
     int head_y = head.y;
-
+    //키 입력받기
     in_Key = getch();
     switch (in_Key) {
       case KEY_LEFT:
@@ -147,10 +167,11 @@ int main()
           stop = true;
           break;
         }
-        
     }
     // 구조물(테두리 벽, 추가된 벽)과 충돌하는지 체크
     if(crash_Check(head_x, head_y)) {break;/*afterCrash(stop);*/} 
+    // (임시) W 누르면 data_wall 실행 -> 맵 바꾸기
+    if(in_Key == 'w') {draw_Wall();}
 
     /* 스네이크 위치 최신화 */
 
@@ -166,12 +187,12 @@ int main()
     /* 출력 코드 */
 
     //맵 배경, 경계선 출력
-    game_Display = newwin(width_game_Display, height_game_Display,  // 맵 크기 50 x 80, (1,1)에서 시작 
+    game_Display = newwin(height_game_Display, width_game_Display,  // 맵 크기 40 x 80, (1,1)에서 시작 
                           X_game_Display, Y_game_Display);
     wbkgd(game_Display, COLOR_PAIR(1)); 
     //wattron(game_Display, COLOR_PAIR(1)); 
-    wborder(game_Display, 'O', 'O', 'O', 'O', // 왼,오,상,하 : color pair 5번
-                          'X', 'X', 'X', 'X'); // 모서리 : color pair 8번
+    wborder(game_Display, 'o', 'o', 'o', 'o', // 왼,오,상,하 : 문자 'o'
+                          'x', 'x', 'x', 'x'); // 모서리 : 문자 'X'
     
     // 맵 구조물 출력
     for(int i=0;i < data_Wall.size();i++){
@@ -197,4 +218,113 @@ int main()
   endwin();
   return 0;
 }
-// commit test
+
+void map_1(){
+  int tmp_x = width_game_Display / 5;
+  int tmp_y = height_game_Display / 2;
+  for (int i = tmp_x; i < tmp_x*4; i++)
+  {
+    data_Wall.push_back(Wall(i, tmp_y));
+  }
+}
+
+void map_2(){
+  int tmp_x = width_game_Display / 5;
+  int tmp_y = height_game_Display / 4 * 3;
+  for (int i = tmp_x; i < tmp_x*4; i++)
+  {
+    data_Wall.push_back(Wall(i, tmp_y));
+  }
+
+  tmp_x = width_game_Display / 5;
+  tmp_y = height_game_Display / 4 * 1;
+  for (int i = tmp_x; i < tmp_x*4; i++)
+  {
+    data_Wall.push_back(Wall(i, tmp_y));
+  }
+}
+
+void map_3(){
+  int tmp_x = width_game_Display / 5;
+  int tmp_y = height_game_Display / 2;
+  for (int i = tmp_x; i < tmp_x*4; i++)
+  {
+    data_Wall.push_back(Wall(i, tmp_y));
+  }
+
+  tmp_x = width_game_Display / 2;
+  tmp_y = height_game_Display / 5;
+  for (int i = tmp_y; i < tmp_y*4; i++)
+  {
+    data_Wall.push_back(Wall(tmp_x, i));
+  }
+}
+
+void map_4(){
+  // 1
+  int tmp_x = width_game_Display / 7;
+  int tmp_y = height_game_Display / 7;
+  for (int i = tmp_x; i < (tmp_x*3 - 8); i++)
+  {
+    data_Wall.push_back(Wall(i, tmp_y));
+  }
+  tmp_x = width_game_Display / 7;
+  tmp_y = height_game_Display / 7;
+  for (int i = tmp_y; i < (tmp_y*3 - 1); i++)
+  {
+    data_Wall.push_back(Wall(tmp_x, i));
+  }
+  // 2
+  tmp_x = width_game_Display / 7;
+  tmp_y = height_game_Display / 7 * 6;
+  for (int i = tmp_x; i < (tmp_x*3 - 8); i++)
+  {
+    data_Wall.push_back(Wall(i, tmp_y));
+  }
+  tmp_x = width_game_Display / 7;
+  tmp_y = height_game_Display / 7 * 6;
+  for (int i = tmp_y; i > (tmp_y/3 + 10); i--)
+  {
+    data_Wall.push_back(Wall(tmp_x, i));
+  }
+  //3
+  tmp_x = width_game_Display / 7 * 6;
+  tmp_y = height_game_Display / 7;
+  for (int i = tmp_x; i > (tmp_x/3 + 16); i--)
+  {
+    data_Wall.push_back(Wall(i, tmp_y));
+  }
+  tmp_x = width_game_Display / 7 * 6;
+  tmp_y = height_game_Display / 7;
+  for (int i = tmp_y; i < (tmp_y*3 - 1); i++)
+  {
+    data_Wall.push_back(Wall(tmp_x, i));
+  }
+  // 4
+  tmp_x = width_game_Display / 7 * 6;
+  tmp_y = height_game_Display / 7 * 6;
+  for (int i = tmp_x; i > (tmp_x/3 + 16); i--)
+  {
+    data_Wall.push_back(Wall(i, tmp_y));
+  }
+  tmp_x = width_game_Display / 7 * 6;
+  tmp_y = height_game_Display / 7 * 6;
+  for (int i = tmp_y; i > (tmp_y/3 + 10); i--)
+  {
+    data_Wall.push_back(Wall(tmp_x, i));
+  }
+  // 중앙 십자모양 벽
+  tmp_x = width_game_Display / 3;
+  tmp_y = height_game_Display / 2;
+  for (int i = tmp_x; i < tmp_x*2; i++)
+  {
+    data_Wall.push_back(Wall(i, tmp_y));
+  }
+
+  tmp_x = width_game_Display / 2;
+  tmp_y = height_game_Display / 3;
+  for (int i = tmp_y; i < tmp_y*2; i++)
+  {
+    data_Wall.push_back(Wall(tmp_x, i));
+  }
+}
